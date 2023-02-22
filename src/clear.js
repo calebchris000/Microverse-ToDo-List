4e3b334619e73a1cd1bc414feb5a55030248f519
calebchris000 <calebchris000@gmail.com>
2023-02-22 07:46:16 +0100

    On clear items, the local storage gets updated

--- src/clear.js
+++ src/clear.js
@@ -4,8 +4,13 @@ export const clearComplete = () => {
   const wrapperChecks = todo.querySelectorAll('.wrapperCheck');
 
   wrapperChecks.forEach((item) => {
-    item.innerHTML = '';
+    item.remove()
   });
+
+  let get = JSON.parse(localStorage.getItem("data"))
+
+  let cleaned = get.filter(x=> x.checked !== true);
+  localStorage.setItem('data', JSON.stringify(cleaned))
 };
 
 export default clearComplete;
